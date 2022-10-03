403d4c99571db803297b15375a29b7b25aee8cd1
Rhian Davies <rhian@jumpingrivers.com>
2022-10-03 19:11:31 +0100

    Run quietly and don't launch browser

--- R/deploy_quarto.R
+++ R/deploy_quarto.R
@@ -4,9 +4,11 @@ deploy_quarto = function(quarto_dir, suppress = suppressMessages) {
   dir.create(tmp_dir, showWarnings = FALSE)
 
   file.copy(file.path(quarto_dir, "index.qmd"), tmp_dir)
-  has_deployed = suppress(quarto::quarto_publish_doc(file.path(tmp_dir, "index.qmd"),
+  has_deployed = suppressMessages(quarto::quarto_publish_doc(file.path(tmp_dir, "index.qmd"),
                              render = "server",
-                             server = Sys.getenv("CONNECT_SERVER")))
+                             server = Sys.getenv("CONNECT_SERVER"),
+                             launch.browser = FALSE,
+                             logLevel = "quiet"))
   return(invisible(has_deployed))
 }
 
