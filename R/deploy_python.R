8a7336599033e5e701c63a2f62173a246ec9efe1
Colin Gillespie <csgillespie@gmail.com>
2022-12-29 16:53:14 +0000

    refactor: Remove {jrApiRStudio} dep

--- R/deploy_python.R
+++ R/deploy_python.R
@@ -2,7 +2,7 @@ deploy_python = function(python_dir,
                          suppress = suppressMessages,
                          rsconnect_type = c("api", "streamlit")) {
   rsconnect_type = match.arg(rsconnect_type)
-  tmp_dir = file.path(tempdir(), "python")
+  tmp_dir = file.path(tempdir(), paste0("python-", rsconnect_type))
   on.exit(cleanup_python(tmp_dir))
   dir.create(tmp_dir, showWarnings = FALSE)
   file.copy(file.path(python_dir, c("app.py", "requirements.txt")), tmp_dir)
@@ -23,6 +23,8 @@ deploy_python = function(python_dir,
                                    "--api-key", get_token(),
                                    "--title-contains", title))
   guid = jsonlite::fromJSON(content$stdout)$guid
+
+
   # Redoing on.exit to clean-up content
   on.exit(cleanup_python(tmp_dir, guid = guid))
   return(invisible(has_deployed$status == 0))
@@ -31,8 +33,11 @@ deploy_python = function(python_dir,
 cleanup_python = function(tmp_dir, guid = NULL) {
   fs::dir_delete(tmp_dir)
   # map in case we make multiple mistakes
-  if (!is.null(guid)) {
-    purrr::map(guid, ~jrApiRStudio::delete_content(.x, server = get_server(), token = get_token()))
-  }
+  con = connectapi::connect(server = get_server(), api_key = get_token()) #nolint
+  purrr::map(guid, ~{
+    item = connectapi::content_item(con, .x)
+    suppressMessages(connectapi::content_delete(item, force = TRUE))
+  })
+
   return(invisible(NULL))
 }
