dc731cd4dd898275bb0a3aa484e0b3232400c172
Colin Gillespie <colin@jumpingrivers.com>
2023-06-11 20:28:32 +0100

    Update

--- R/check_server.R
+++
@@ -1,9 +0,0 @@
-check_server_version = function(server, token, debug_level) {
-  suppress = get_suppress(debug_level)
-  cli::cli_h2("Checking Server Version")
-  client = suppress(connectapi::connect(server = server, api_key = token))
-
-  server_version = client$server_settings()$version
-  audit.base::audit_server_version(server_version = server_version, type = "connect")
-  return(server_version)
-}
