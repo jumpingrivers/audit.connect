acdcce14aa5666d565ca902d88a60b6a63c5f670
Colin Gillespie <colin@jumpingrivers.com>
2023-10-01 13:41:18 +0100

    feat: Check for Posit name leakage

--- R/check_server_headers.R
+++
@@ -1,9 +0,0 @@
-check_server_headers = function() {
-  res = try(serverHeaders::check(get_server(clean = TRUE)), silent = TRUE)
-  if ("try-error" %in% class(res))  {
-    cli::cli_alert_warning("Attempt to reach server using {cli::col_red('http')} failed.")
-    cli::cli_alert_info("Trying again using {cli::col_blue('https')}")
-    res = serverHeaders::check(get_server())
-  }
-  return(res)
-}
